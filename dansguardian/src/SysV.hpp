2b0006f66dce2d76dcf0b04ece9ec9befd53d202
ognam <ognam@mycompany.com>
2005-10-12 12:15:57 +0000

    Style guide conformance. Functions added/tweaked to facilitate new -Q command-line option.
    git-svn-id: https://svn.code.sf.net/p/dansguardian/code/trunk@51 c6e6bffc-5bc6-4360-8735-ace3d95093af

--- dansguardian/src/SysV.hpp
+++ dansguardian/src/SysV.hpp
@@ -19,26 +19,36 @@
 
 #ifndef __HPP_SYSV
 #define __HPP_SYSV
-#include <sys/types.h>
-#include <string>
+
+
+// INCLUDES
+
+#include "platform.h"
+
 #include "OptionContainer.hpp"
 
-class SysV {
+#include <sys/types.h>
+#include <string>
 
-public:
-    int kill(std::string pidfile);
-    int showPID(std::string pidfile);
-    int openPIDFile(std::string pidfile);
-    int writePIDFile(int pidfilefd);
-    bool amIRunning(std::string pidfile);
-    int hup(std::string pidfile);
-    int usr1(std::string pidfile);
 
-private:
-    pid_t getPID(std::string pidfile);
-    pid_t getPIDFromFile(std::string pidfile);
-    pid_t getPIDFromCommand(const char* command);
-    bool confirmName(pid_t p);
-};
+// DECLARATIONS
+
+// Kill the process specified in the given pidfile, optionally deleting the pidfile while we're at it,
+// along with the UNIX domain sockets for the old logger & url cache
+int sysv_kill(std::string pidfile, bool dounlink = true);
+
+// show PID of running DG process
+int sysv_showpid(std::string pidfile);
+// check that the process in the pidfile is running
+bool sysv_amirunning(std::string pidfile);
+
+// delete any existing file with this name, and create a new one with relevant mode flags
+int sysv_openpidfile(std::string pidfile);
+// write our pid to the given file & close it
+int sysv_writepidfile(int pidfilefd);
+
+// send HUP or USR1 to the process in the pidfile
+int sysv_hup(std::string pidfile);
+int sysv_usr1(std::string pidfile);
 
 #endif
