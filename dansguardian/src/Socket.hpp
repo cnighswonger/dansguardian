7934be229667daa52bd8012ebae7512ec8c65a92
ognam <ognam@mycompany.com>
2005-10-13 09:22:08 +0000

    Adding missing licenses to comment blocks
    git-svn-id: https://svn.code.sf.net/p/dansguardian/code/trunk@62 c6e6bffc-5bc6-4360-8735-ace3d95093af

--- dansguardian/src/Socket.hpp
+++ dansguardian/src/Socket.hpp
@@ -1,5 +1,8 @@
 // Socket class - implements BaseSocket for INET domain sockets
 
+//Please refer to http://dansguardian.org/?page=copyright2
+//for the license for this code.
+
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
--- dansguardian/src/UDSocket.cpp
+++ dansguardian/src/UDSocket.cpp
@@ -1,5 +1,8 @@
 // UDSocket class - implements BaseSocket for UNIX domain sockets
 
+//Please refer to http://dansguardian.org/?page=copyright2
+//for the license for this code.
+
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
