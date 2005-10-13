f04e732ab46fe0fd607a0afce2958fa2fe773ec4
ognam <ognam@mycompany.com>
2005-10-13 11:48:04 +0000

    Style guide conformance & minor tidyups. OptionContainer no longer passed in as a pointer (probably a relic of the initial attempt at libtoolisation); defined as an extern as per everywhere else.
    git-svn-id: https://svn.code.sf.net/p/dansguardian/code/trunk@64 c6e6bffc-5bc6-4360-8735-ace3d95093af

--- dansguardian/src/downloadmanagers/default.cpp
+++ dansguardian/src/downloadmanagers/default.cpp
@@ -1,64 +1,102 @@
+// Default download manager, used when no other plugin matches the user agent
+
+//Please refer to http://dansguardian.org/?page=copyright2
+//for the license for this code.
+
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+// INCLUDES
+
 #include "../DownloadManager.hpp"
-#include "../String.hpp"
-#include "../DataBuffer.hpp"
-#include "../Socket.hpp"
-#include "../HTTPHeader.hpp"
 #include "../OptionContainer.hpp"
-#include "../platform.h"
+
 #include <syslog.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-class dminstance : public DMPlugin {
+
+// GLOBALS
+
+extern OptionContainer o;
+
+
+// DECLARATIONS
+
+class dminstance:public DMPlugin
+{
 public:
-	dminstance( ConfigVar & definition );
-	int in(OptionContainer *o, DataBuffer *d, Socket *sock, Socket *peersock, HTTPHeader *requestheader, HTTPHeader *docheader, bool wantall, int *headersent, bool *toobig);
+	dminstance(ConfigVar & definition);
+	int in(DataBuffer * d, Socket * sock, Socket * peersock, HTTPHeader * requestheader,
+		HTTPHeader * docheader, bool wantall, int *headersent, bool * toobig);
 // uncomment these if you wish to replace the default inherited functions
-//	int init(int dgversion);
-//	int quit(void);
-//	add class variables for storage here
+//      int init(int dgversion);
+//      int quit(void);
+//      add class variables for storage here
 private:
-	 ConfigVar cv;
+	  ConfigVar cv;
 };
 
-dminstance::dminstance( ConfigVar & definition ): DMPlugin( definition ) {
-	cv = definition;
-	return;
-};
+
+// IMPLEMENTATION
 
 // class factory code *MUST* be included in every plugin
-DMPlugin* defaultdmcreate( ConfigVar & definition ) {
+
+DMPlugin *defaultdmcreate(ConfigVar & definition)
+{
 #ifdef DGDEBUG
 	std::cout << "Creating default DM" << std::endl;
 #endif
-	return new dminstance( definition ) ;
+	return new dminstance(definition);
 }
 
-void defaultdmdestroy(DMPlugin* p) {
+void defaultdmdestroy(DMPlugin * p)
+{
 #ifdef DGDEBUG
 	std::cout << "Destroying default DM" << std::endl;
 #endif
 	delete p;
 }
 
+dminstance::dminstance(ConfigVar & definition):DMPlugin(definition)
+{
+	cv = definition;
+	return;
+};
+
 // end of Class factory
 
 
 // uncomment these if you wish to replace the default inherited functions
-//int dminstance::init(int dgversion) {
-//	return 0;
-//}
-//int dminstance::quit(void) {
-//	return 0;
-//}
 
 // < 0 = error
 // = 0 = ok
 // > 0 = warning
 
+//int dminstance::init(int dgversion) {
+//      return 0;
+//}
+//int dminstance::quit(void) {
+//      return 0;
+//}
 
-int dminstance::in(OptionContainer *o, DataBuffer *d, Socket *sock, Socket *peersock, class HTTPHeader *requestheader, class HTTPHeader *docheader, bool wantall, int *headersent, bool *toobig) {
+// download body for this request
+int dminstance::in(DataBuffer * d, Socket * sock, Socket * peersock, class HTTPHeader * requestheader,
+	class HTTPHeader * docheader, bool wantall, int *headersent, bool * toobig)
+{
 
 	//DataBuffer *d = where to stick the data back into
 	//Socket *sock = where to read from
@@ -67,80 +105,77 @@ int dminstance::in(OptionContainer *o, DataBuffer *d, Socket *sock, Socket *peer
 	//HTTPHeader *docheader = header used for sending first line of reply
 	//bool wantall = to determine if just content filter or a full scan
 	//int *headersent = to use to send the first line of header if needed
-	//				  or to mark the header has already been sent
+	//                                or to mark the header has already been sent
 	//bool *toobig = flag to modify to say if it could not all be downloaded
 
-	#ifdef DGDEBUG
-		std::cout << "Inside default download manager plugin" << std::endl;
-	#endif
+#ifdef DGDEBUG
+	std::cout << "Inside default download manager plugin" << std::endl;
+#endif
 
 //  To access settings for the plugin use the following example:
-//	std::cout << "cvtest:" << cv["dummy"] << std::endl;
+//      std::cout << "cvtest:" << cv["dummy"] << std::endl;
 
 	int rc, newsize;
 
-	char* block;  // buffer for storing a grabbed block from the
-				  // imput stream
-	char* temp;
+	char *block;  // buffer for storing a grabbed block from the
+	// imput stream
+	char *temp;
 
 	bool swappedtodisk = false;
 
 	struct timeval themdays;
 	struct timeval nowadays;
-	struct timezone notused;
-	gettimeofday(&themdays, &notused);
+	gettimeofday(&themdays, NULL);
 
-	while(true) {
+	while (true) {
 		// send x-header keep-alive here
-		if (o->trickle_delay > 0) {
-			gettimeofday(&nowadays, &notused);
-			if (nowadays.tv_sec - themdays.tv_sec > o->trickle_delay) {
+		if (o.trickle_delay > 0) {
+			gettimeofday(&nowadays, NULL);
+			if (nowadays.tv_sec - themdays.tv_sec > o.trickle_delay) {
 				themdays.tv_sec = nowadays.tv_sec;
 				if ((*headersent) < 1) {
-					#ifdef DGDEBUG
-						std::cout << "sending first line of header first" << std::endl;
-					#endif
+#ifdef DGDEBUG
+					std::cout << "sending first line of header first" << std::endl;
+#endif
 					docheader->out(peersock, __DGHEADER_SENDFIRSTLINE);
 					(*headersent) = 1;
 				}
-				#ifdef DGDEBUG
-					std::cout << "trickle delay - sending X-DGKeepAlive: on" << std::endl;
-				#endif
+#ifdef DGDEBUG
+				std::cout << "trickle delay - sending X-DGKeepAlive: on" << std::endl;
+#endif
 				peersock->writeString("X-DGKeepAlive: on\r\n");
 			}
 		}
 
 		if (wantall) {
 			if (!swappedtodisk) {
-				if (d->buffer_length > o->max_content_ramcache_scan_size) {
-					if (d->buffer_length > o->max_content_filecache_scan_size) {
+				if (d->buffer_length > o.max_content_ramcache_scan_size) {
+					if (d->buffer_length > o.max_content_filecache_scan_size) {
 						(*toobig) = true;
 						break;
-					}
-					else {
+					} else {
 						d->tempfilefd = d->getTempFileFD();
 						if (d->tempfilefd < 0) {
-							#ifdef DGDEBUG
-								std::cerr << "error buffering to disk so skipping disk buffering" << std::endl;
-							#endif
-							syslog(LOG_ERR, "%s","error buffering to disk so skipping disk buffering");
+#ifdef DGDEBUG
+							std::cerr << "error buffering to disk so skipping disk buffering" << std::endl;
+#endif
+							syslog(LOG_ERR, "%s", "error buffering to disk so skipping disk buffering");
 
 							(*toobig) = true;
 							break;
 						}
-						d->writeEINTR(d->tempfilefd, d->data, d->buffer_length);
+						writeEINTR(d->tempfilefd, d->data, d->buffer_length);
 						swappedtodisk = true;
 						d->tempfilesize = d->buffer_length;
-						#ifdef DGDEBUG
-							std::cout << "swapping to disk" << std::endl;
-						#endif
+#ifdef DGDEBUG
+						std::cout << "swapping to disk" << std::endl;
+#endif
 					}
 
 				}
 			}
-		}
-		else {
-			if (d->buffer_length > o->max_content_filter_size) {
+		} else {
+			if (d->buffer_length > o.max_content_filter_size) {
 				(*toobig) = true;
 				break;
 			}
@@ -148,8 +183,7 @@ int dminstance::in(OptionContainer *o, DataBuffer *d, Socket *sock, Socket *peer
 
 		if (d->buffer_length >= 32768) {
 			newsize = d->buffer_length;
-		}
-		else {
+		} else {
 			newsize = 32768;
 		}
 
@@ -157,33 +191,33 @@ int dminstance::in(OptionContainer *o, DataBuffer *d, Socket *sock, Socket *peer
 			block = new char[newsize];
 			try {
 				sock->checkForInput(d->timeout);
-			}  catch (exception& e) {
-				delete[] block;
+			} catch(exception & e) {
+				delete[]block;
 				break;
 			}
-		// improved more efficient socket read which uses the buffer better
+			// improved more efficient socket read which uses the buffer better
 			rc = d->bufferReadFromSocket(sock, block, newsize, d->timeout);
 			// grab a block of input, doubled each time
 
 			if (rc < 1) {
-				delete[] block;
+				delete[]block;
 				break;  // an error occured so end the while()
-					// or none received so pipe is closed
+				// or none received so pipe is closed
 			}
 			if (rc > 0) {
-			   temp = new char[d->buffer_length + rc];  // replacement store
-			   memcpy(temp, d->data, d->buffer_length);  // copy the current data
-			   memcpy(temp + d->buffer_length, block, rc);  // copy the new data
-			   delete[] d->data;  // delete the current data block
-			   d->data = temp;
-			   d->buffer_length += rc;  // update data size counter
+				temp = new char[d->buffer_length + rc];  // replacement store
+				memcpy(temp, d->data, d->buffer_length);  // copy the current data
+				memcpy(temp + d->buffer_length, block, rc);  // copy the new data
+				delete[]d->data;  // delete the current data block
+				d->data = temp;
+				d->buffer_length += rc;  // update data size counter
 			}
-			delete[] block;
-		}
-		else {
+			delete[]block;
+		} else {
 			try {
 				sock->checkForInput(d->timeout);
-			}  catch (exception& e) {
+			}
+			catch(exception & e) {
 				break;
 			}
 			rc = d->bufferReadFromSocket(sock, d->data, d->buffer_length, d->timeout);
@@ -192,26 +226,26 @@ int dminstance::in(OptionContainer *o, DataBuffer *d, Socket *sock, Socket *peer
 			}
 			if (rc > 0) {
 				lseek(d->tempfilefd, 0, SEEK_END);  // not really needed
-				d->writeEINTR(d->tempfilefd, d->data, rc);
+				writeEINTR(d->tempfilefd, d->data, rc);
 				d->tempfilesize += rc;
-				#ifdef DGDEBUG
-					std::cout << "written to disk:" << rc << " total:" << d->tempfilesize << std::endl;
-				#endif
+#ifdef DGDEBUG
+				std::cout << "written to disk:" << rc << " total:" << d->tempfilesize << std::endl;
+#endif
 			}
 		}
 	}
 
-	if (!toobig && !swappedtodisk) {  // won't deflate stuff swapped to disk
+	if (!toobig && !swappedtodisk) {	// won't deflate stuff swapped to disk
 		if (d->decompress.contains("deflate")) {
-			#ifdef DGDEBUG
-				std::cout << "zlib format" << std::endl;
-			#endif
+#ifdef DGDEBUG
+			std::cout << "zlib format" << std::endl;
+#endif
 			d->zlibinflate(false);  // incoming stream was zlib compressed
 		}
 		else if (d->decompress.contains("gzip")) {
-			#ifdef DGDEBUG
-				std::cout << "gzip format" << std::endl;
-			#endif
+#ifdef DGDEBUG
+			std::cout << "gzip format" << std::endl;
+#endif
 			d->zlibinflate(true);  // incoming stream was gzip compressed
 		}
 	}
