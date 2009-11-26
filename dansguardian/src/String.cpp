95f16ecdabcac1887471ce3452099ca2a6b62ad5
ognam <ognam@mycompany.com>
2009-11-26 14:50:25 +0000

    Make startsWith method more sensible
    git-svn-id: https://svn.code.sf.net/p/dansguardian/code/trunk@1072 c6e6bffc-5bc6-4360-8735-ace3d95093af

--- dansguardian/src/String.cpp
+++ dansguardian/src/String.cpp
@@ -288,10 +288,7 @@ void String::hexDecode()
 // does this string start with the given text?
 bool String::startsWith(const String& s) const
 {
-	if (!strncmp(this->c_str(), s.c_str(), s.length())) {
-		return true;
-	}
-	return false;
+	return (strncmp(this->c_str(), s.c_str(), s.length()) == 0);
 }
 
 // does this string end with the given text?
