f6eed7b76c1fa7296172fe5ffd69ff1f3939eb2d
dansguardian <dansguardian@mycompany.com>
2011-07-13 12:27:09 +0000

    - Tidied up licensing notices and removed some email addresses.
    git-svn-id: https://svn.code.sf.net/p/dansguardian/code/trunk@1126 c6e6bffc-5bc6-4360-8735-ace3d95093af

--- dansguardian/src/HTTPHeader.cpp
+++ dansguardian/src/HTTPHeader.cpp
@@ -1,25 +1,6 @@
-//Please refer to http://dansguardian.org/?page=copyright2
-//for the license for this code.
-//Written by Daniel Barron (daniel@//jadeb.com).
-//For support go to http://groups.yahoo.com/group/dansguardian
-
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-//This file contains modifications suggested and mostly provided by
-//Daniel Robbins 13/4/01 drobbins@gento.org
-//Modifications include, but not limited to, getcontenttype(), << , >>
+// For all support, instructions and copyright go to:
+// http://dansguardian.org/
+// Released under the GPL v2, with the OpenSSL exception described in the README file.
 
 
 // INCLUDES
